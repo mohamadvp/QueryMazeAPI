eb239a3a4d0e305f4af51fc6699b75c51bcacefc
mohammad <s.mohammadreza.lol@gmail.com>
2025-06-08 14:30:40 +0330

    setup the model

--- querymaze/models.py
+++ querymaze/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Product (models.Model):
+    sku = models.CharField(max_length=20, primary_key=True)
+    desc = models.CharField(max_length=288)
+    wholesale_cost = models.DecimalField(max_digits=10, decimal_places=3)
+    dims_cm = models.CharField(max_length=288)
+
+    def __str__(self):
+        return f"{self.sku} - {self.desc}"
+
+
+class Customer (models.Model):
+    customerid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=288)
+    address = models.CharField(max_length=288)
+    citystatezip = models.CharField(max_length=288)
+    birthdate = models.DateField()
+    phone = models.IntegerField()
+    timezone = models.CharField(max_length=288)
+    lat = models.FloatField()
+    long = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order (models.Model):
+    orderid = models.IntegerField(primary_key=True)
+    customerid = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    ordered = models.DateField()
+    shipped = models.DateField()
+    items = models.TextField(blank=True)
+    total = models.DecimalField(max_digits=20, decimal_places=3)
+
+    def __str__(self):
+         return f"Order #{self.orderid}"
+
+
+class OrderItem(models.Model):
+    orderid = models.ForeignKey(Order , on_delete=models.CASCADE)
+    sku = models.ForeignKey(Product, on_delete=models.CASCADE)
+    qty = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=20, decimal_places=3)
+
+    def __str__(self):
+        return f"{self.qty} x {self.product.sku} for Order #{self.order.orderid}"
