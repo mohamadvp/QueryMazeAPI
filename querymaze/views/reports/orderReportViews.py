1e115b8bde57c69dabdca1c45ecf7f35aa1d5f67
mohammad <s.mohammadreza.lol@gmail.com>
2025-08-13 11:32:44 +0330

    fix order report api

--- querymaze/views/reports/orderReportViews.py
+++ querymaze/views/reports/orderReportViews.py
@@ -1,11 +1,19 @@
 from django.db.models import Prefetch
-from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from ...serializers.reportSerializers import OrderReportSerializer
 from ...models import Order, OrderItem
 
-class OrderReportApiView(ListAPIView):
-    serializer_class = OrderReportSerializer
 
-    def get_queryset(self):
-        return Order.objects.filter(ordered__range=('2020-01-01', '2021-01-01')).select_related('customer').prefetch_related(
+class OrderReportApiView(APIView):
+
+    def get(self, request):
+        start_data = request.query_params.get('start')
+        end_date = request.query_params.get('end')
+
+        queryset = Order.objects.filter(ordered__date__range=[start_data,end_date]).select_related('customer').prefetch_related(
             Prefetch('orderitem_set', queryset=OrderItem.objects.select_related('product')))
+        
+        serializer = OrderReportSerializer(queryset, many=True)
+
+        return Response(serializer.data)
